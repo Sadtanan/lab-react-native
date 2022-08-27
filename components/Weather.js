5616e64c73576f64a6e6b0de1c930cfbefa11ef4
Sadtanan <youhtzaa@hotmail.com>
2022-08-27 21:27:35 +0700

    Connect

--- components/Weather.js
+++ components/Weather.js
@@ -2,6 +2,23 @@ import React from "react";
 import { Text } from 'react-native'
 
 export default function Weather(props) {
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=6e0fa53a969736d7e82fa22426665bf2`)
+            .then((response) => response.json())
+            .then((json) => {
+                setForecastInfo({
+                    main: json.weather[0].main,
+                    description: json.weather[0].description,
+                    temp: json.main.temp
+                });
+            })
+        .catch((error) => {
+            console.warn(error);
+        });
+        }
+    }, [props.zipCode])
     const [forecastInfo, setForecastInfo] = useState({
         main: '-',
         description: '-',
