1d7775eb2bfd8b93940261ff2fef733882ba57da
Sadtanan <youhtzaa@hotmail.com>
2022-08-27 21:12:18 +0700

    Components and Image Background

--- components/Weather.js
+++ components/Weather.js
@@ -2,8 +2,26 @@ import React from "react";
 import { Text } from 'react-native'
 
 export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
+        description: '-',
+        temp: 0
+    })
     return (
-        <Text>{props.zipCode}</Text>
+        <View>
+            <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+                <Text>Zip Code</Text>
+                <Text>{props.zipCode}</Text>
+                <Forecast {...forecastInfo} />
+            </ImageBackground>
+        </View>
     );
 }
+const styles = StyleSheet.create({
+    backdrop: {
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+    },
+});
    
